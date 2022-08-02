b9f7c5f0a621e0a9ed6b714742558031bdaeb739
saijeevanpuchakayala <saijeevan2002@gmail.com>
2022-08-02 18:20:06 +0530

    index page added and major UI changes

--- FarmWiserApp/flaskApp.py
+++ FarmWiserApp/flaskApp.py
@@ -1,79 +1,6 @@
 from flask import Flask, render_template, request
-import requests
-import config
-import pickle
 import numpy as np
-
-
-categoricalValues = {
-    20.0: "rice",
-    11.0: "maize",
-    3.0: "chickpea",
-    9.0: "kidneybeans",
-    18.0: "pigeonpeas",
-    13.0: "mothbeans",
-    14.0: "mungbean",
-    2.0: "blackgram",
-    10.0: "lentil",
-    19.0: "pomegranate",
-    1.0: "banana",
-    12.0: "mango",
-    7.0: "grapes",
-    21.0: "watermelon",
-    15.0: "muskmelon",
-    0.0: "apple",
-    16.0: "orange",
-    17.0: "papaya",
-    4.0: "coconut",
-    6.0: "cotton",
-    8.0: "jute",
-    5.0: "coffee",
-}
-
-
-def weather_fetch(city_name):
-    api_key = config.weather_api_key
-    base_url = "http://api.openweathermap.org/data/2.5/weather?"
-
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-    response = requests.get(complete_url.replace(" ", ""))
-    x = response.json()
-
-    if x["cod"] != "404":
-        y = x["main"]
-
-        temperature = round((y["temp"] - 273.15), 2)
-        humidity = y["humidity"]
-        return temperature, humidity
-    else:
-        return None
-
-
-def cropPredictor(inputData, model):
-    if model == "Random Forest":
-        model = pickle.load(open("./ml_models/CR_RF.pkl", "rb"))
-        pred = model.predict(inputData)
-        # print("RF")
-        return pred[0]
-
-    elif model == "Decision Tree":
-        model = pickle.load(open("./ml_models/CR_DecisionTree.pkl", "rb"))
-        pred = model.predict(inputData)
-        # print("DT")
-        return pred[0]
-
-    elif model == "Naive Bayes":
-        model = pickle.load(open("./ml_models/CR_NaiveBayes.pkl", "rb"))
-        pred = model.predict(inputData)
-        # print("NB")
-        return pred[0]
-
-    elif model == "XGBoost":
-        model = pickle.load(open("./ml_models/CR_XB.pkl", "rb"))
-        xbpred = model.predict(inputData)
-        pred = categoricalValues[xbpred[0]]
-        # print("XB")
-        return pred
+from utils import *
 
 
 app = Flask(__name__)
@@ -82,13 +9,20 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     title = "FarmWiser"
-    return render_template("index.html", title=title)
+    news = newsExtracter()
+    return render_template("index.html", title=title, news=news)
 
 
-@app.route("/weather")
-def weather():
+@app.route("/cropRecommender")
+def cropRecommender():
     title = "FarmWiser"
-    return render_template("weather.html", title=title)
+    return render_template("cropRecommender.html", title=title)
+
+
+@app.route("/weatherFinder")
+def weatherFinder():
+    title = "FarmWiser"
+    return render_template("weatherFinder.html", title=title)
 
 
 @app.route("/cropRecommended", methods=["POST"])
---
+++ FarmWiserApp/utils.py
@@ -0,0 +1,127 @@
+import requests
+import config
+import pickle
+from bs4 import BeautifulSoup
+import cfscrape
+
+
+scraper = cfscrape.create_scraper()
+
+
+class News_Scraper:
+    def __init__(self, keywords):
+        self.markup = requests.get(
+            "https://economictimes.indiatimes.com/news/economy/agriculture"
+        ).text
+        self.keywords = keywords
+
+    def parse(self):
+        soup = BeautifulSoup(self.markup, "lxml")
+        links = soup.findAll("a", limit=300)
+        self.news_links = []
+        self.news_linksTexts = []
+        for link in links:
+            for keyword in self.keywords:
+                if keyword in link.text and "?" not in link.text:
+                    self.news_links.append(link)
+        for link in self.news_links:
+            self.news_linksTexts.append(link.text)
+
+
+def newsExtracter():
+    s = News_Scraper(
+        [
+            "agriculture",
+            "farming",
+            "demand",
+            "prices",
+            "government",
+            "crop",
+            "production",
+            "increase",
+            "decrease",
+            "rainfall",
+            "weather",
+            "market prices",
+        ]
+    )
+
+    news_text_list = []
+    news_links_list = []
+    s.parse()
+    for x in s.news_links:
+        news_text_list.append(x.text)
+        news_links_list.append("https://economictimes.indiatimes.com" + x["href"])
+    news = list(zip(news_text_list, news_links_list))
+    return list(set(news))
+
+
+categoricalValues = {
+    20.0: "rice",
+    11.0: "maize",
+    3.0: "chickpea",
+    9.0: "kidneybeans",
+    18.0: "pigeonpeas",
+    13.0: "mothbeans",
+    14.0: "mungbean",
+    2.0: "blackgram",
+    10.0: "lentil",
+    19.0: "pomegranate",
+    1.0: "banana",
+    12.0: "mango",
+    7.0: "grapes",
+    21.0: "watermelon",
+    15.0: "muskmelon",
+    0.0: "apple",
+    16.0: "orange",
+    17.0: "papaya",
+    4.0: "coconut",
+    6.0: "cotton",
+    8.0: "jute",
+    5.0: "coffee",
+}
+
+
+def weather_fetch(city_name):
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url.replace(" ", ""))
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+
+
+def cropPredictor(inputData, model):
+    if model == "Random Forest":
+        model = pickle.load(open("./ml_models/CR_RF.pkl", "rb"))
+        pred = model.predict(inputData)
+        # print("RF")
+        return pred[0]
+
+    elif model == "Decision Tree":
+        model = pickle.load(open("./ml_models/CR_DecisionTree.pkl", "rb"))
+        pred = model.predict(inputData)
+        # print("DT")
+        return pred[0]
+
+    elif model == "Naive Bayes":
+        model = pickle.load(open("./ml_models/CR_NaiveBayes.pkl", "rb"))
+        pred = model.predict(inputData)
+        # print("NB")
+        return pred[0]
+
+    elif model == "XGBoost":
+        model = pickle.load(open("./ml_models/CR_XB.pkl", "rb"))
+        xbpred = model.predict(inputData)
+        pred = categoricalValues[xbpred[0]]
+        # print("XB")
+        return pred
